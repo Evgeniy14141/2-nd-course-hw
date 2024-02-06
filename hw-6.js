97c7a11748bd192cb65d08de6ac352906421c95e
Evgeniy14141 <eugene-gr14@yandex.ru>
2024-02-06 23:46:44 +0400

    HW6_modification

--- hw-6.js
+++ hw-6.js
@@ -1,11 +1,12 @@
 // Задание 1:
-
+ 
 const numbs = [1, 5, 4, 10, 0, 3];
-
-for (let i = 0; i < numbs.length; i++) {
-	if (numbs[i] == 10) break;
-	console.log(numbs[i]);
-}  
+for (с of numbs) {
+console.log(с);
+if (с === 10) {
+    break;   
+}
+}
 
 
 
@@ -16,6 +17,7 @@ const indexNumbs = [1, 5, 4, 10, 0, 3];
 	console.log(indexNumbs.indexOf(4));
 
 
+
 // Задание 3:
 
 let numbersWithSpace = [1, 3, 5, 10, 20];
@@ -24,19 +26,21 @@ numbersWithSpace = numbersWithSpace.join(' ');
 console.log(numbersWithSpace);
 
 
+
 // Задание 4:
 
-let threeArrays = [
-	[1, 1, 1],
-	[1, 1, 1],
-	[1, 1, 1],    
- ];
- 
- for (let threeArraysIn of threeArrays) {
-	 for (let element of threeArraysIn) {
-		 console.log(element);
-	 }
- }
+let arr = [];
+
+for (let i = 0; i < 3; i++) {
+	arr[i] = []; // создаем подмассив
+	
+	for (let j = 0; j < 3; j++) {
+		arr[i][j] = 1 // заполняем подмассив числами
+	}
+}
+
+console.log(arr);
+
 
 
  // Задание 5:
@@ -45,6 +49,7 @@ let pro = [1, 1, 1];
 
 pro.push(2, 2, 2);
 console.log(pro);
+
 
 
 // Задание 6:
@@ -56,70 +61,77 @@ sorting.pop();
 console.log(sorting);
 
 
-// Задание 7: 
 
-alert([9, 8, 7, 6, 5].includes(+prompt('Угадай число')) ? 'Угадал' : 'Не угадал');
+// Задание 7:
+
+let numbi = [9, 8, 7, 6, 5];
+let answ = false;
+
+do {
+    let quo = prompt('Введите число');
+
+    for (let i = 0; i < numbi.length; i++) {
+        if (numbi[i] === Number(quo)) {
+            answ = true;
+        }
+    }
+
+    if (answ) {
+        alert('Угадал');
+    } else {
+        alert('Не угадал');
+    }
+} while (!answ);
+
 
 
 //Задание 8:
 
 let wordsRev = 'abcdef';
-let wordsCut = wordsRev.split('');
-
-wordsCut = wordsCut.reverse();
-
-wordsCut = wordsCut.join('');
+let wordsCut = wordsRev.split('').reverse().join('');
     console.log(wordsCut);
 
 
 
 // Задание 9:
 
-const nested = arrs = [
-    [1, 2, 3],
-    [4, 5, 6]
-];
-
-const flattenArray = arr => {
-    const getDepth = a => Array.isArray(a) ?
-        1 + Math.max(...a.map(getDepth)) : 0
-    return arr.flat(getDepth(arr))
-}
-console.log(flattenArray(nested))
+let arr1 = [[1, 2, 3,],[4, 5, 6]];
+console.log(arr1.flat());
 
 
 
-// Задание 10:
+// Задание 10: 
 
-let sequentialArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-let sum = 0;
-for (let i = 0; i < sequentialArray.length; i++) {
-    sum += sequentialArray[i];
+let sequentialArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; i < sequentialArray.length - 1; i++) {
+  let num = sequentialArray[i];
+  let num2 = sequentialArray[i + 1];
+  let sum = num + num2;
+  console.log(`Сумма ${num} и ${num2} равна ${sum}`);
 }
 
-console.log('Sum is ' + sum)
 
-
-
-// Задание 11:
-
-let fiveArr = [1, 2, 3, 4, 5]
-fiveArr = fiveArr.map(el => el ** 2);
-
-    console.log(fiveArr);
-
-
+// Задание 11: 
+  
+let fiveArr = [1, 2, 3, 4, 5];
+  
+let = squareNum =fiveArr.map(xx=>(xx ** 2))
+  
+console.log(squareNum);
 	
-// Задание 12: 
 
-function getLengthWords(arr) {
-    let newarr = [];
-    for (let i = 0; i < arr.length; i++) {
-       newarr.push( arr[i].length);
-    }  return newarr;
-}
 
-getLengthWords(['слово', '', 'слог', 'длинное предложение', 'буква']); // [5, 0, 4, 19, 5]
+// Задание 12:
+
+function getLengthWords(arm) {
+    return arm.map(aword => aword.length);
+  }
+  
+  let words = ['слово', '', 'слог', 'длинное предложение', 'буква'];
+  let wordLeng = getLengthWords(words);
+  console.log(wordLeng);
+
 
 
 // Задание 13:
